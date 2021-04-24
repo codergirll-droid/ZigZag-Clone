ce851e92ddafeb9e9f3c32653b03b878167bbcb6
Ayşe Oduncu <oduncu981@gmail.com>
2021-04-24 16:42:35 +0300

    Complete game

---
+++ ZigZagClone/Assets/Scripts/CharController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CharController : MonoBehaviour
+{
+    public Transform rayStart;
+    public GameObject crystalEffect;
+
+
+    private Rigidbody rb;
+    private bool walkingRight = true;
+    private Animator anim;
+    private GameManager GameManager;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        GameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!GameManager.gameStarted)
+        {
+            return;
+        }
+        else
+        {
+            anim.SetTrigger("gameStarted");
+        }
+
+        rb.transform.position = transform.position + transform.forward * 2 * Time.deltaTime;
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Switch();
+        }
+
+        RaycastHit hit;
+
+        if(!Physics.Raycast(rayStart.position, -transform.up, out hit, Mathf.Infinity))
+        {
+            anim.SetTrigger("isFalling");
+        }
+        else
+        {
+            anim.SetTrigger("notFallingAnymore");
+        }
+
+
+        if (transform.position.y < -2)
+        {
+            GameManager.EndGame();
+        }
+    }
+    private void Switch()
+    {
+        if (!GameManager.gameStarted)
+        {
+
+            return;
+        }
+        walkingRight = !walkingRight;
+        if (walkingRight)
+            transform.rotation = Quaternion.Euler(0, 45, 0);
+        else
+            transform.rotation = Quaternion.Euler(0, -45, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Crystal")
+        {
+            
+            GameManager.IncreaseScore();
+
+            GameObject g = Instantiate(crystalEffect, rayStart.transform.position, Quaternion.identity);
+            Destroy(g, 2);
+            Destroy(other.gameObject);
+        }
+    }
+    
+}
---
+++ ZigZagClone/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameStarted;
+    public int score;
+    public Text scoreText;
+    public Text highScoreText;
+
+
+    private void Awake()
+    {
+        highScoreText.text = "Best: " + getHighscore().ToString();
+    }
+
+    public void Startgame()
+    {
+        gameStarted = true;
+        FindObjectOfType<Road>().StartBuilding();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Startgame();
+        }
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+
+        if (score > getHighscore())
+        {
+            PlayerPrefs.SetInt("Highscore", score);
+            highScoreText.text = "Best: " + score.ToString();
+        }
+
+    }
+    public int getHighscore()
+    {
+        int i = PlayerPrefs.GetInt("Highscore");
+        return i;
+    }
+
+}
